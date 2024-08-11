7edadfbebc97702eaa00062e6acdfbdde59471f8
RichestsoftSukhbir <sukhbir.richestsoft@gmail.com>
2024-08-11 13:19:44 +0530

    2024-08-11T13:19:44+05:30

--- index.js
+++ index.js
@@ -3,24 +3,37 @@ import moment from 'moment';
 import simpleGit from 'simple-git';
 
 const path = './data.json';
-const date = moment().subtract(5, 'd').format();
 
-const data = { date };
+const markCommit = (x, y) => {
+    const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
+    const data = {
+        date: date
+    }
 
-async function run() {
-  try {
-    await jsonfile.writeFile(path, data, { spaces: 2 });
-
-    const git = simpleGit();
-
-    await git.add('.'); // ← this adds ALL changes
-    await git.commit(date, { '--date': date });
-    await git.push();
-
-    console.log('✅ Everything pushed!');
-  } catch (err) {
-    console.error('❌ Git push failed:', err);
-  }
+    jsonfile.writeFile(path, data, () => {
+        simpleGit().add('.').commit(date, { '--date': date }).push()
+    })
 }
 
-run();
+markCommit(2, 3);
+// const data = { date };
+
+// async function run() {
+//   try {
+//     await jsonfile.writeFile(path, data, () => {
+        
+//     });
+
+//     const git = simpleGit();
+
+//     await git.add('.'); // ← this adds ALL changes
+//     await git.commit(date, { '--date': date });
+//     await git.push();
+
+//     console.log('✅ Everything pushed!');
+//   } catch (err) {
+//     console.error('❌ Git push failed:', err);
+//   }
+// }
+
+// run();
