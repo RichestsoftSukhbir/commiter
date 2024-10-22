906daccdea780f9f2228c1f5f6fdd2a42e9287ac
RichestsoftSukhbir <sukhbir.richestsoft@gmail.com>
2024-10-22 14:35:01 +0530

    2024-10-22T14:35:01+05:30

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 import jsonfile from 'jsonfile';
 import moment from 'moment';
+import random from 'random';
 import simpleGit from 'simple-git';
 
 const path = './data.json';
 
-const markCommit = (x, y) => {
+const markCommit = (n) => {
+
+    if(n===0) return simpleGit().push();
+
+    const x = random.int(0,54);
+    const y = random.int(0,6);
+
     const date = moment().subtract(1, "y").add(1, "d").add(x, "w").add(y, "d").format();
     const data = {
         date: date
     }
 
+    console.log(date);
+
     jsonfile.writeFile(path, data, () => {
-        simpleGit().add('.').commit(date, { '--date': date }).push()
+        simpleGit().add('.').commit(date, { '--date': date }, markCommit.bind(this, --n));
     })
 }
 
-markCommit(2, 3);
+markCommit(100);
 // const data = { date };
 
 // async function run() {
